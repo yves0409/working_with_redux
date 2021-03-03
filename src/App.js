240cb7ec2b6670a4c2935c4a60d19eaa333ab729
Yves Loeys <yves.loeys@gmail.com>
2021-03-02 18:27:59 -0800

    basic app setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import "./App.css";
+import "./styles.css";
+import React, { useState } from "react";
+import Fields from "./components/Fields";
+import Languages from "./components/Languages";
+import Translate from "./components/Translate";
 
-function App() {
+export default function App() {
+  const [language, setLanguage] = useState("ru");
+  const [text, setText] = useState("");
+
   return (
-    <div className="App">
-      <h1>Init</h1>
+    <div>
+      <Fields label="Enter English" onChange={setText} value={text} />
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
     </div>
   );
 }
-
-export default App;
